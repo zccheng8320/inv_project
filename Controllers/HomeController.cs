594c5674604c366849088d2e2527da99ad861561
Chang <zccheng8320@gmail.com>
2019-07-30 17:21:09 +0800

    2019-07-30

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using INV_Project.Models;
 namespace INV_Project.Controllers
 {
     public class HomeController : Controller
     {
+        private invEntities db = new invEntities();
         public ActionResult Index()
         {
             return View();
@@ -15,8 +17,14 @@ namespace INV_Project.Controllers
 
         public ActionResult Customer()
         {
-           
-            return View();
+            var a = TempData["CUST_CODE"];
+            var p = (from d in db.CUSTOMER select d).OrderByDescending(d => d.CUST_CODE).FirstOrDefault();
+            if (a != null)
+                p = (from d in db.CUSTOMER where d.CUST_CODE == a select d).FirstOrDefault();
+            else
+                TempData["CUST_CODE"] = p.CUST_CODE;
+            Debug.WriteLine(p.CUST_CODE);
+            return View(p);
         }
 
         public ActionResult Contact()
